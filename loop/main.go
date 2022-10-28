8c134c1725e8f2c5b3c612835376a0d06663414e
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-10-28 14:15:50 +0800

    字数读取，range关键字

--- loop/main.go
+++ loop/main.go
@@ -19,5 +19,19 @@ func main() {
 		fmt.Printf("i=%v\n", i)
 	}
 
+	shalom := "shalom"
+	// 这里的len函数返回的是该字符串的bytes
+	// 如果要获取字符串的长度的话要导入unicode/utf8包
+	// 使用utf8.RuneCountInString函数来获取长度
+	for i := 0; i < len(shalom); i++ {
+		fmt.Printf("%c\n", shalom[i])
+	}
+
+	// 要遍历某个类型的话可以使用range关键字
+	// 该关键字是一个迭代器的语法糖,可以遍历各种集合
+	for i, c := range shalom {
+		fmt.Printf("i=%v c=%c\n", i, c)
+	}
+
 	fmt.Println("循环结束")
 }
