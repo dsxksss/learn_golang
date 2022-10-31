a119dda2c2dad00bdbee6e60d4f655611f74b333
VentCat <73477803+dsxksss@users.noreply.github.com>
2022-10-31 11:37:43 +0800

    函数，可变参函数，多返回值函数，函数的声明定义

---
+++ function/main.go
@@ -0,0 +1,35 @@
+package main
+
+import "errors"
+
+// 如果两个参数类型一样可以指表明后面参数的类型
+// 函数名大写表示public 小写表示private
+func max(a, b int) int {
+	if a > b {
+		return a
+	} else {
+		return b
+	}
+}
+
+// 函数可以返回多个返回值
+// func maxE(a, b int) (result int, err error) {
+func maxE(a, b int) (int, error) {
+	if a > b {
+		return a, nil
+	} else if a < b {
+		return b, nil
+	}
+	return -1, errors.New("发生错误")
+}
+
+// 可变参数函数
+// ...表示函数参数的数量是可变的
+// interface{} 表示空接口 类似于ts中的any
+func printx(a ...interface{}) (interface{}, error) {
+	return a, errors.New("我是一个可变参数函数")
+}
+
+func main() {
+
+}
